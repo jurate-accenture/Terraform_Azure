22b5e90a61a23f54a93cb5d9b30a49a406838c8c
jurate-accenture <jurate.zizyte@accenture.com>
2023-11-10 11:34:13 +0100

    adding VM

--- main.tf
+++ main.tf
@@ -18,3 +18,60 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.Terraform_Azure.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes    = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "network_interface" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.Terraform_Azure.location
+  resource_group_name = azurerm_resource_group.Terraform_Azure.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "myVM"
+  location              = azurerm_resource_group.Terraform_Azure.location
+  resource_group_name   = azurerm_resource_group.Terraform_Azure.name
+  network_interface_ids = [azurerm_network_interface.network_interface.id]
+
+  size       = "Standard_D2s_v3"
+ 
+
+   
+  disable_password_authentication = false
+  admin_username                   = "jurate"  # Replace with your username
+  admin_password                   = "myvm177A:q/"  # Replace with your desired password
+ 
+    
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
